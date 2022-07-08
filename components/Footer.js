ec8db7a981001e0056394d082b82984acd65b02f
Mathieu Bonhommeau <mat.bonhommeau85@gmail.com>
2022-07-08 14:38:28 +0200

    integrate header and footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,39 @@
 import React from "react";
 import Style from "../styles/Footer.module.scss";
 import PropTypes from "prop-types";
+import homeIcon from "../assets/images/home-icon.png";
+import galleryIcon from "../assets/images/gallery-icon.png";
+import mailIcon from "../assets/images/mail-icon.png";
+import menuIcon from "../assets/images/menu-icon.png";
+import Image from "next/image";
 
-const Footer = (props) => {
-  return <footer className={Style.footer}>footer</footer>;
+const Footer = () => {
+  return (
+    <footer className={Style.footer}>
+      <div>
+        <Image
+          src={homeIcon}
+          alt="Icone accueil"
+          width={30}
+          height={30}
+        ></Image>
+      </div>
+      <div>
+        <Image
+          src={galleryIcon}
+          alt="Icone gallery"
+          width={30}
+          height={30}
+        ></Image>
+      </div>
+      <div>
+        <Image src={mailIcon} alt="Icone mail" width={30} height={30}></Image>
+      </div>
+      <div>
+        <Image src={menuIcon} alt="Icone menu" width={30} height={30}></Image>
+      </div>
+    </footer>
+  );
 };
 
 Footer.propTypes = {};
--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Style from "../styles/Header.module.scss";
-import logo from "../assets/logo.png";
+import logo from "../assets/images/logo.png";
+import avatar from "../assets/images/avatar.png";
 import Image from "next/image";
 
 const Header = (props) => {
@@ -8,10 +9,13 @@ const Header = (props) => {
     <header className={Style.header}>
       <div className={Style.logoContainer}>
         <Image src={logo} alt="Logo Lilou draws" width={30} height={30} />
-        <div>
+        <div className={Style.logoText}>
           <span className={Style.logoMainText}>LILOU</span>
           <span className={Style.logoSubmainText}>Draws</span>
         </div>
+      </div>
+      <div>
+        <Image src={avatar} alt="Avatar user account" width={30} height={30} />
       </div>
     </header>
   );
