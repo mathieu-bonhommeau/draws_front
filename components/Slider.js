d639af7df1738427e280d57ccab8f7692b0c4670
Mathieu Bonhommeau <mat.bonhommeau85@gmail.com>
2022-08-01 18:58:31 +0200

    adapt datas

--- components/Slider.js
+++ components/Slider.js
@@ -5,74 +5,159 @@ import NameBorder from "../assets/images/nameSliderImage.png";
 import Image from "next/image";
 import datas from "../mockDatas/datas.js";
 
-const styleByPosition = [
-    {
-        position: 0,
-        style: {
-            transform: "perspective(200px) translateZ(-250px) translateX(305px) rotateY(-100deg)",
-            transition: "transform ease 1s, opacity ease 1s"
-        }
-    },{
-        position: 1,
-        style: {
-            transform: "perspective(200px) translateZ(-100px) translateX(10px) rotateY(-35deg)",
-            transition: "transform ease 1s, opacity ease 1s"
-        }
-    },{
-        position: 2,
-        style: {
-            transform: "perspective(200px) translateZ(-25px)",
-            transition: "transform ease 1s, opacity ease 1s"
-        }
-    },{
-        position: 3,
-        style: {
-            transform: "perspective(200px) translateZ(-100px) translateX(-10px) rotateY(35deg)",
-            transition: "transform ease 1s, opacity ease 1s"
-        }
-    },{
-        position: 4,
-        style: {
-            transform: "perspective(200px) translateZ(-250px)  translateX(-305px) rotateY(100deg)",
-            transition: "transform ease 1s, opacity ease 1s"
-        }
-    }
-]
+const initialStyleForPosition = [
+  {
+    position: 0,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-250px) translateX(305px) rotateY(-100deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 1,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-250px) translateX(305px) rotateY(-100deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 2,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-250px) translateX(305px) rotateY(-100deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 3,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-100px) translateX(10px) rotateY(-35deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 4,
+    style: {
+      transform: "perspective(200px) translateZ(-25px)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 5,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-100px) translateX(-10px) rotateY(35deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 6,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-250px)  translateX(-305px) rotateY(100deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 7,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-250px) translateX(305px) rotateY(-100deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 8,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-250px) translateX(305px) rotateY(-100deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+  {
+    position: 9,
+    style: {
+      transform:
+        "perspective(200px) translateZ(-250px) translateX(305px) rotateY(-100deg)",
+      transition: "transform ease 1s, opacity ease 1s",
+    },
+  },
+];
 
 const Slider = (props) => {
-
   const [sliderState, setSliderState] = useState([]);
 
+  const [styleByPosition, setStyleByPosition] = useState(
+    initialStyleForPosition
+  );
+  console.log(styleByPosition);
   useEffect(() => {
+    const adaptDatasForSlider = [
+      datas[3],
+      datas[4],
+      ...datas,
+      datas[0],
+      datas[1],
+      datas[2],
+    ];
     setSliderState(
-      datas.map((d, i) => {
+      [...adaptDatasForSlider].map((d, i) => {
         return {
           ...d,
-          position: i,
+          actualPosition: i,
         };
       })
     );
   }, []);
 
-  console.log(sliderState);
   const slider = useRef();
 
   const prevClick = (e) => {
-      setSliderState([...sliderState].map(e => {
-          return {
-              ...e,
-              position: e.position === 0 ? 4 : e.position - 1
-          }
-      }))
+    setStyleByPosition(
+      [...styleByPosition].map((e) => {
+        return {
+          ...e,
+          style: {
+            ...e.style,
+            transform: `translateX(-250px) ${e.style.transform}`,
+          },
+        };
+      })
+    );
+    setSliderState(
+      [...sliderState].map((e) => {
+        return {
+          ...e,
+          actualPosition: e.actualPosition === 0 ? 9 : e.actualPosition - 1,
+        };
+      })
+    );
   };
 
   const nextClick = (e) => {
-    setSliderState([...sliderState].map(e => {
+    setStyleByPosition(
+      [...styleByPosition].map((e) => {
         return {
-            ...e,
-            position: e.position === 4 ? 0 : e.position + 1
-    }
-    }))
+          ...e,
+          style: {
+            ...e.style,
+            transform:
+              e.position === 0 ? "" : `translateX(250px) ${e.style.transform}`,
+          },
+        };
+      })
+    );
+    setSliderState(
+      [...sliderState].map((e) => {
+        return {
+          ...e,
+          actualPosition: e.actualPosition === 9 ? 0 : e.actualPosition + 1,
+        };
+      })
+    );
   };
 
   return (
@@ -80,9 +165,17 @@ const Slider = (props) => {
       <div className={Style.slider} ref={slider}>
         {sliderState === []
           ? ""
-          : sliderState.map((e) => {
+          : sliderState.map((e, i) => {
               return (
-                <div className={Style.drawItemContainer} style={styleByPosition.find(el => e.position === el.position).style} key={e.id}>
+                <div
+                  className={Style.drawItemContainer}
+                  style={
+                    styleByPosition.find(
+                      (el) => e.actualPosition === el.position
+                    ).style
+                  }
+                  key={e.id + "-" + i}
+                >
                   <div className={Style.draw}>
                     <Image
                       src={`/mockPictures/${e.picture}`}
